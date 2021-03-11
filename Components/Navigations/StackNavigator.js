a33382791113761e0426c86cf449762437508d78
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2021-03-11 15:47:53 +0500

    sdfs

--- Components/Navigations/StackNavigator.js
+++ Components/Navigations/StackNavigator.js
@@ -1,51 +1,34 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { createStackNavigator } from '@react-navigation/stack';
-import {Home} from "../Home"
+import { Home } from "../Home"
 import { View } from 'react-native';
 import { UpperTabNavigation } from "../TeamStatus/UpperTabNavigator";
 import LeagueIdContext from '../team/Context/mCTX';
+import { GetLeague } from "../Utility/UI";
 
 const Stack = createStackNavigator();
 
-const getLeague = (leagueId) => {
-    switch(leagueId)
-    {
-      case 140:
-        return {
-          title : "La Liga"
+export const MyStackNavigator = () => {
+
+
+  const mCTX = useContext(LeagueIdContext);
+
+  return (
+    <Stack.Navigator initialRouteName="TabNav" >
+      <Stack.Screen name="TabNav" component={UpperTabNavigation} options={{
+        title: mCTX[0] !== null ? GetLeague(mCTX[0]).title : "",
+        headerStyle: {
+          backgroundColor: mCTX[0] !== null ? GetLeague(mCTX[0]).color : "",
+
+        },
+        headerTitleStyle: {
+          fontWeight: 'bold',
+          color: '#FFFFFF',
+
         }
-        case 78:
-          return{
-            title: "Bundesliga"
-          }
-      default:
-        return{
-          title : "La Liga"
-        }
-    }
-}
-
-export const  MyStackNavigator = () => {
+      }} />
+    </Stack.Navigator>
+  );
 
 
-  let LeagueCTX = useContext(LeagueIdContext);
-  
-    return (
-      <Stack.Navigator initialRouteName="TabNav" >
-        <Stack.Screen name="TabNav" component={UpperTabNavigation} options={{
-            title : LeagueCTX[0] !== null ? getLeague(LeagueCTX[0]).title : 0,
-            headerStyle: {
-                backgroundColor: '#6802cf',
-                
-            },
-            headerTitleStyle: {
-                fontWeight: 'bold',
-                color: '#FFFFFF',
-              
-            }
-        }} />
-      </Stack.Navigator>
-    );
-
- 
 }

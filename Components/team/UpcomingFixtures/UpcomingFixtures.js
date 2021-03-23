88601ad4194c6bfcfbd5b618152569565ab594f5
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2021-03-23 15:25:53 +0500

    sadasd

--- Components/team/UpcomingFixtures/UpcomingFixtures.js
+++ Components/team/UpcomingFixtures/UpcomingFixtures.js
@@ -2,45 +2,45 @@ import React, { useEffect, useState } from 'react'
 import { Image, Text, View } from 'react-native'
 import { CardFixture } from './CardFixture'
 import LaLiga_Logo from "../../../RawData/images/laliga.png";
-
+import { bubbleSortByTime } from "../../Utility/updateFixtureArray";
 
 export const UpcomingFixtures = () =>
 {
    
-
-    const [myData, setMyData] = useState([])
+    const [isLoading, setIsLoading] = useState(true);
+    const [myData, setMyData] = useState([]);
 
     const fetchData = [
         {
             title: "CHAMPIONS LEAGUE",
             logo: LaLiga_Logo,
-            data: null
+            data: []
         },
         {
             title: "EUROPA LEAGUE",
             logo: LaLiga_Logo,
-            data: null
+            data: []
         }
         ,
         {
             title: "LaLiga",
             logo: LaLiga_Logo,
-            data: null
+            data: []
         },
         {
             title: "ENGLISH PREMIER LEAGUE",
             logo: LaLiga_Logo,
-            data: null
+            data: []
         },
         {
             title: "BUNDESLIGA",
             logo: LaLiga_Logo,
-            data: null
+            data: []
         },
         {
             title: "SERIE A",
             logo: LaLiga_Logo,
-            data: null
+            data: []
         }
     ]
 
@@ -48,7 +48,7 @@ export const UpcomingFixtures = () =>
         () => {
             async function getLeagueId() {
                 
-                const response = await fetch(`https://v3.football.api-sports.io/fixtures?season=2020`, {
+                const response = await fetch(`https://v3.football.api-sports.io/fixtures?season=2020&date=2021-03-23`, {
                     "method": "GET",
                     "headers": {
                         "x-rapidapi-host": "v3.football.api-sports.io",
@@ -78,40 +78,48 @@ export const UpcomingFixtures = () =>
 
     useEffect(
         () => {
-            if(myData)
+            if(myData.length())
             {
+                console.log("Array is not empty second side effect")
                 for (let i = 0; i < myData.length; i++) {
                     const element = myData[i];
                     
                     switch (element.league.id) {
                         case 140:
-                            // laliga
-                            fetchData[2].data = element
+                            // Laliga
+                            fetchData[2].data.push(element)
                             break;
                         case 30:
-                            // epl
-                            fetchData[3].data = element
+                            // Epl
+                            fetchData[3].data.push(element)
                             break;
                         case 135:
-                            // serie a
-                            fetchData[5].data = element
+                            // Serie a
+                            fetchData[5].data.push(element)
                             break;
                         case 78:
-                           // budesliga 
-                           fetchData[4].data = element
+                           // Budesliga
+                           fetchData[4].data.push(element)
                             break;
                         case 2:
-                            // champion league
-                            fetchData[0].data = element
+                            // Champion league
+                            fetchData[0].data.push(element)
                             break;
                         case 3:
                            // Europa league 
-                           fetchData[1].data = element
+                           fetchData[1].data.push(element)
                             break;
                         
                         default:
                             break;
                     }
+                }
+
+                setIsLoading(!isLoading)
+
+                for(const e of fetchData)
+                {
+                    console.log("data : ", e.data)
                 }
             }
         },
